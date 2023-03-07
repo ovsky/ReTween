7aafa7fa412a0020b42c6eeebd4cbb2471d51199
Przemysław Orłowski <przemek.orlowski@outlook.com>
2023-03-07 21:19:42 +0100

    Fix EaseTypeDrawer, to draw only boxed types

--- Assets/ReTween/Scripts/Editor/EaseTypeDrawer.cs
+++ Assets/ReTween/Scripts/Editor/EaseTypeDrawer.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 namespace ReTween.Editor
 {
-    [CustomPropertyDrawer(typeof(Easing))]
+    [CustomPropertyDrawer(typeof(EaseType))]
     public class EaseTypeDrawer : PropertyDrawer
     {
         [SerializeField] AnimationCurve curve;
         [SerializeField] int lastType = -1;
 
-        const float ACCURACY = 100f;
+        const float ACCURACY = 50f;
         const float BUTTON_WIDTH = 30f;
         const float PREVIEW_HEIGHT = 50f;
 
@@ -21,6 +21,19 @@ namespace ReTween.Editor
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             GUILayout.Space(-20);
+
+            // Check the depth of the property, the 'Ease' class has a depth of 1 and the 'EaseType' has a depth of 0 (the root).
+            // So we can simply check if the user is trying to draw the EaseType directly and warn him about it. 
+
+            // This is a temporary solution, I will try to find a better way to handle this. 
+            // I'm not sure that the depth will be 0 at the most situations in this type of GUI drawing... 
+            // ...but probably it will be beacuse it will be the root.
+            if (property.depth == 0)
+            {
+                EditorGUILayout.PrefixLabel($"{property.displayName} [EaseType : {property.propertyPath}]");
+                EditorGUILayout.HelpBox("Drawing the raw EaseType is currently disabled to avoid architectural misunderstandings. If you are looking for a class that has a pretty interface for easing, try to use the 'Ease' instead of 'EaseType', beacise it is the major type used for simple easing setups.", MessageType.Error);
+                return;
+            }
 
             EditorGUI.BeginProperty(position, label, property);
             EditorGUILayout.BeginHorizontal();
@@ -49,7 +62,7 @@ namespace ReTween.Editor
 
         private void DrawPreview(SerializedProperty property)
         {
-            if (property.isExpanded && (property.enumValueIndex != (int)Easing.Custom))
+            if (property.isExpanded && (property.enumValueIndex != (int)EaseType.Custom))
             {
                 if (lastType != property.enumValueIndex)
                 {
@@ -57,20 +70,16 @@ namespace ReTween.Editor
 
                     for (int i = 0; i < ACCURACY; i++)
                     {
-                        float x = EaseCalculator.Calculate((Easing)property.enumValueIndex, i / ACCURACY);
-                        Keyframe key = new Keyframe(i / ACCURACY, x);
-
-                        if (i > 0)
-                        {
-                            key.inTangent = curve.keys[i - 1].value;
-                            key.outTangent = curve.keys[i - 1].value;
-                        }
+                        float y = EaseCalculator.Calculate((EaseType)property.enumValueIndex, i / ACCURACY);
+                        Keyframe key = new Keyframe(i / ACCURACY, y);
 
                         curve.AddKey(key);
+                        curve.SmoothTangents(i, 1f);
                     }
                     lastType = property.enumValueIndex;
                 }
                 EditorGUILayout.CurveField(curve, GUILayout.Height(PREVIEW_HEIGHT));
+                EditorGUILayout.Separator();
             }
         }
     }
